c41a571ac3110569564a67ef522bbb0bd30a60a8
Nick Hammes <220595@student.fontys.nl>
2017-11-24 15:50:51 +0100

    Added student class, changed form

--- EmoSphereDemo/EmoSphereDemo/Program.cs
+++ EmoSphereDemo/EmoSphereDemo/Program.cs
@@ -16,7 +16,7 @@ namespace EmoSphereDemo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new GameForm());
         }
     }
 }
